344ee02237bdf7ac03206817aaba53a1c80095dc
KhacTri <trink@vng.com.vn>
2021-08-11 21:59:51 +0700

    update client.cpp and server.cpp, now they can transmit video over UDP socket.

--- client.cpp
+++ client.cpp
@@ -1,48 +1,74 @@
-// Client side implementation of UDP client-server model
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-  
+#include <iostream>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/videoio/videoio_c.h>
+
 #define PORT 8080
-#define MAXLINE 1024
-  
-int main() {
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv ) {
+    //declare for socket
     int sockfd;
-    char buffer[MAXLINE];
-    char *hello = "Hello from client";
     struct sockaddr_in servaddr;
-  
-    // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-  
-    memset(&servaddr, 0, sizeof(servaddr));
-      
+    socklen_t addr_len;
+    int buffer_size, bytes_sent = 0;
+    //declare for camera, from Opencv
+    Mat image;
+    VideoCapture videoCapture;
+    vector<uchar> buffer;
+
+    memset(&servaddr, 0, sizeof(servaddr));  
     // Filling server information
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(PORT);
     servaddr.sin_addr.s_addr = INADDR_ANY;
       
-    int n;
-    socklen_t len = sizeof(servaddr);
     
-    sendto(sockfd, (const char *)hello, strlen(hello),
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
-            sizeof(servaddr));
-    printf("Hello message sent.\n");
-          
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-                MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-    buffer[n] = '\0';
-    printf("Server : %s\n", buffer);
-  
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    namedWindow("Video Capture", WINDOW_AUTOSIZE ); 
+    videoCapture.open(0); //open camera
+
+    while (true) 
+    {
+        videoCapture.read(image);
+        imshow("Video Capture", image);
+
+        //encode Mat to vector
+        imencode(".jpg", image, buffer);
+
+        buffer_size = buffer.size();
+        
+        //send buffer size to Server
+        addr_len = sizeof(servaddr);
+        if (bytes_sent = sendto(sockfd, &buffer_size, sizeof(buffer_size),
+            MSG_CONFIRM, (struct sockaddr *) &servaddr, addr_len) < 0) 
+            {
+                cout << "send error" << endl;
+            }
+        else
+            {   
+                bytes_sent = sendto(sockfd, &buffer[0], buffer.size(),
+                    MSG_CONFIRM, (struct sockaddr *) &servaddr, addr_len);
+                // cout << bytes_sent << ";" << bytes_sent - buffer_size << endl; //print for debug
+            }      
+        if(waitKey(30) >= 0) break;
+    }     
+    
     close(sockfd);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,61 +1,88 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-// Server side implementation of UDP client-server model
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-  
+#include <iostream>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/videoio/videoio_c.h>
+
+#define PORT 8080
+
+using namespace std;
+using namespace cv;
+
+using namespace std;
+using namespace cv;
+
 #define PORT 8080
 #define MAXLINE 1024
   
 // Driver code
 int main() {
     int sockfd;
-    char buffer[MAXLINE];
-    char *hello = "Hello from server";
-    struct sockaddr_in servaddr, cliaddr;
-      
+    struct sockaddr_in client_addr;
+    socklen_t addr_len;
+    int buffer_size, bytes_recv = 0;
+    vector<uchar> buffer;
+    Mat image;
+
+    memset(&client_addr, 0, sizeof(client_addr));
+    // Filling server information
+    client_addr.sin_family    = AF_INET; // IPv4
+    client_addr.sin_addr.s_addr = INADDR_ANY;
+    client_addr.sin_port = htons(PORT);
+
+    namedWindow("Video Playback", WINDOW_AUTOSIZE); //set name and size of window
+
     // Creating socket file descriptor
     if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
-      
-    memset(&servaddr, 0, sizeof(servaddr));
-    memset(&cliaddr, 0, sizeof(cliaddr));
-      
-    // Filling server information
-    servaddr.sin_family    = AF_INET; // IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(PORT);
+    
       
     // Bind the socket with the server address
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr, 
-            sizeof(servaddr)) < 0 )
+    if ( bind(sockfd, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0 )
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
       
-    int n;
-    socklen_t len = sizeof(cliaddr);
-  
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-                MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
-    buffer[n] = '\0';
-    printf("Client : %s\n", buffer);
-    sendto(sockfd, (const char *)hello, strlen(hello), 
-        MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
-    printf("Hello message sent.\n"); 
+
+    while (true)
+    {   
+        addr_len = sizeof(client_addr);
+        //Recieves the byte size of the client file.
+        if (bytes_recv = recvfrom(sockfd, &buffer_size, sizeof(int), 
+            MSG_WAITALL, (struct sockaddr *) &client_addr, &addr_len) < 0)  
+            {
+                cout << "receive error" << endl;
+            }
+        else {
+            try {
+            buffer.resize(buffer_size);
+            if (bytes_recv = recvfrom(sockfd, &buffer[0], buffer.size(), 
+                MSG_WAITALL, (struct sockaddr *) &client_addr, &addr_len) > 0)
+                {  
+                // cout << bytes_sent << ";" << bytes_sent - buffer_size << endl; //print for debug    
+                
+                //decode buffer to Mat, then show the image
+                image= imdecode(buffer, 1);
+                imshow("Video Playback", image);
+                }
+            }
+            catch (...) {
+                // cerr << e.msg << endl;
+            }
+            if(waitKey(30) >= 0) break;
+        }
+    }
+         
       
     return 0;
 }
