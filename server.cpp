7ecc5b00c357066210f24b0143d38cbc7474f2a0
khactrist99 <65272790+khactrist99@users.noreply.github.com>
2021-08-19 17:01:12 +0700

    Update server.cpp
    fix format

--- server.cpp
+++ server.cpp
@@ -7,13 +7,13 @@
 unsigned char buffer[BUFFER_SIZE];
 
 int recv_data(void *ptr){
-    Network network;
-    network.Init();
+	Network network;
+	network.Init();
 
-    bool running = true;
-    while(running)
-    {
-        network.recv(buffer, BUFFER_SIZE);
+	bool running = true;
+	while(running)
+	{
+		network.recv(buffer, BUFFER_SIZE);
 		SDL_Event event;
 		event.type = REFRESH_EVENT;
 		SDL_PushEvent( &event );
@@ -22,23 +22,23 @@ int recv_data(void *ptr){
 }
 
 int main() {
-    Interface UI;
-    UI.Init();
+	Interface UI;
+	UI.Init();
 	SDL_Thread *refresh_thread = SDL_CreateThread(recv_data,NULL, NULL);
-	
+
 	// SDL_Event event;
-    bool running = true;
-    SDL_Event event;
+	bool running = true;
+	SDL_Event event;
 	while(running){
 		//Wait
 		SDL_PollEvent(&event);
 		if(event.type==REFRESH_EVENT){
-            UI.Render(buffer);
+			UI.Render(buffer);
 			SDL_Delay(33);
 		}else if(event.type==SDL_QUIT){
 			running=false;
 		}
 	}
 	SDL_Quit();
-    return 0;
+	return 0;
 }
